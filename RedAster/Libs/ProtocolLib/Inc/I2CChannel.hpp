d62d0bc81ddca09bb68ee1fa91d4dfffd80ba768
alberto-cardini <albycardini@gmail.com>
2025-02-23 14:46:49 +0100

    Major refactor to properly use the API. (not actively tested).

--- RedAster/Libs/ProtocolLib/Inc/I2CChannel.hpp
+++ RedAster/Libs/ProtocolLib/Inc/I2CChannel.hpp
@@ -19,34 +19,36 @@ extern "C" {
 
 class I2CChannel {
 public:
-    explicit I2CChannel(I2C_HandleTypeDef init, uint32_t polling_timeout = 0)
-    : handler(std::make_unique<I2C_HandleTypeDef>(init)), polling_t(polling_timeout) {}
+    explicit I2CChannel(I2C_HandleTypeDef init, uint32_t sData = 32, uint32_t timeout_tx = 100, uint32_t timeout_rx = 100)
+    : handler(std::make_unique<I2C_HandleTypeDef>(init)), sData(sData), timeout_tx(timeout_tx), timeout_rx(timeout_rx) {}
 
     /*
      * @brief: Allow to transmit in non-blocking and polling mode implementing the two methods given by the HAL.
      *         The protocol use DMA when transmits in non-blocking mode.
      * @param: Specify if SLAVE or MASTER as transmission role.
      * @param: Specify if non-blocking or polling mode as transmission mode.
-     * @param: Buffer to transmit. THE FIRST ELEMENT OF THE BUFFER MUST BE THE TARGET DEVICE ADDRESS.
+     * @param: Buffer to transmit. THE FIRST ELEMENT OF THE BUFFER MUST BE THE TARGET DEVICE REGISTER.
      *
      * @retval: HAL status.
      */
-    HAL_StatusTypeDef Transmit(Identity role, Mode transmit_mode, std::vector<uint8_t>& buffer) const;
+    HAL_StatusTypeDef transmit(uint8_t &chip_address, std::vector<uint8_t>& buffer, Mode transmit_mode = Mode::POLLING, Identity role = Identity::MASTER) const;
 
     /*
      * @brief: Allow reception in non-blocking and polling mode implementing the two methods given by the HAL.
      *         The protocol use DMA when receives in non-blocking mode.
      * @param: Specify if SLAVE or MASTER as reception role.
      * @param: Specify if non-blocking or polling mode as reception mode.
-     * @param: Buffer to store the data received . THE FIRST ELEMENT OF THE BUFFER MUST BE THE TARGET DEVICE ADDRESS.
+     * @param: Buffer to store the data received. THE FIRST ELEMENT OF THE BUFFER MUST BE THE TARGET DEVICE ADDRESS.
      *
      * @retval: HAL status.
      */
-    HAL_StatusTypeDef Receive(Identity role, Mode receive_mode, std::vector<uint8_t>& buffer);
+    HAL_StatusTypeDef receive(uint8_t &chip_address, std::vector<uint8_t>& buffer, Mode receive_mode = Mode::POLLING, Identity role = Identity::MASTER);
 
 private:
     std::unique_ptr<I2C_HandleTypeDef> handler;
-    uint32_t polling_t;
+    const uint32_t sData;
+    uint32_t timeout_tx;
+    uint32_t timeout_rx;
 };
 
 #endif //REDASTER_I2C_HPP
--- RedAster/Libs/ProtocolLib/Src/I2CChannel.cpp
+++ RedAster/Libs/ProtocolLib/Src/I2CChannel.cpp
@@ -3,49 +3,53 @@
 //
 
 #include "I2CChannel.hpp"
+// Remember that the first element of the transmitted buffer has to be the register in which you want to write the info.
+HAL_StatusTypeDef I2CChannel::transmit(uint8_t &chip_address, std::vector<uint8_t> &buffer, Mode transmit_mode, Identity role) const {
 
-HAL_StatusTypeDef I2CChannel::Transmit(Identity role, Mode transmit_mode, std::vector<uint8_t> &buffer) const {
+  HAL_StatusTypeDef status;
 
-    HAL_StatusTypeDef status;
+  if (HAL_I2C_GetState(handler.get()) == HAL_I2C_STATE_READY) {
 
     if (role == Identity::MASTER && transmit_mode == Mode::POLLING)
-        status = HAL_I2C_Master_Transmit(handler.get(), buffer.front(), get_valid_ptr(buffer), size_in_byte(buffer), polling_t);
+      status = HAL_I2C_Master_Transmit(handler.get(), chip_address << 1, buffer.data(), buffer.size() * sizeof(uint8_t),timeout_tx);
 
     else if (role == Identity::MASTER && transmit_mode == Mode::NON_BLOCKING)
-        status = HAL_I2C_Master_Transmit_DMA(handler.get(), buffer.front(), get_valid_ptr(buffer), size_in_byte(buffer));
+      status = HAL_I2C_Master_Transmit_DMA(handler.get(), chip_address << 1, buffer.data(), buffer.size() * sizeof(uint8_t));
 
     else if (role == Identity::SLAVE && transmit_mode == Mode::POLLING)
-        status = HAL_I2C_Slave_Transmit(handler.get(), get_valid_ptr(buffer), size_in_byte(buffer), polling_t);
+      status = HAL_I2C_Slave_Transmit(handler.get(), buffer.data(), buffer.size() * sizeof(uint8_t), timeout_tx);
 
     else if (role == Identity::SLAVE && transmit_mode == Mode::NON_BLOCKING)
-        status = HAL_I2C_Slave_Transmit_DMA(handler.get(), get_valid_ptr(buffer), size_in_byte(buffer));
+      status = HAL_I2C_Slave_Transmit_DMA(handler.get(), buffer.data(), buffer.size() * sizeof(uint8_t));
+    else status = HAL_ERROR;
 
-    else
-        status = HAL_ERROR;
-        //throw std::logic_error("I2C transmission failed");
+  } else status = HAL_ERROR;
 
-    return status;
+  return status;
 }
 
-HAL_StatusTypeDef I2CChannel::Receive(Identity role, Mode receive_mode, std::vector<uint8_t> &buffer) {
+HAL_StatusTypeDef I2CChannel::receive(uint8_t &chip_address, std::vector<uint8_t> &buffer, Mode receive_mode, Identity role) {
 
-    HAL_StatusTypeDef status;
+  HAL_StatusTypeDef status;
+  buffer.resize(sData);
+
+  if (HAL_I2C_GetState(handler.get()) == HAL_I2C_STATE_READY) {
 
     if (role == Identity::MASTER && receive_mode == Mode::POLLING)
-        status = HAL_I2C_Master_Receive(handler.get(), buffer.front(), get_valid_ptr(buffer), size_in_byte(buffer), polling_t);
+      status = HAL_I2C_Master_Receive(handler.get(), (chip_address << 1) | 0x01, buffer.data(), buffer.size() * sizeof(uint8_t), timeout_rx);
 
     else if (role == Identity::MASTER && receive_mode == Mode::NON_BLOCKING)
-        status = HAL_I2C_Master_Receive_DMA(handler.get(), buffer.front(), get_valid_ptr(buffer), size_in_byte(buffer));
+      status = HAL_I2C_Master_Receive_DMA(handler.get(), (chip_address << 1) | 0x01, buffer.data(), buffer.size() * sizeof(uint8_t));
 
     else if (role == Identity::SLAVE && receive_mode == Mode::POLLING)
-        status = HAL_I2C_Slave_Receive(handler.get(), get_valid_ptr(buffer), size_in_byte(buffer), polling_t);
+      status = HAL_I2C_Slave_Receive(handler.get(), buffer.data(), buffer.size() * sizeof(uint8_t), timeout_rx);
 
     else if (role == Identity::SLAVE && receive_mode == Mode::NON_BLOCKING)
-        status = HAL_I2C_Slave_Receive_DMA(handler.get(), get_valid_ptr(buffer), size_in_byte(buffer));
+      status = HAL_I2C_Slave_Receive_DMA(handler.get(), buffer.data(), buffer.size() * sizeof(uint8_t));
 
-    else
-        status = HAL_ERROR;
-        //throw std::logic_error("I2C transmission failed");
+    else status = HAL_ERROR;
 
-    return status;
+  }else status = HAL_ERROR;
+
+  return status;
 }
